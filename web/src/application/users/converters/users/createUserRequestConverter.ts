d01a1432eaff04eacb6f11f68230aef9da933df2
anthonylowther <anthonylowther@icloud.com>
2022-01-17 08:36:22 -0600

    anthony broke stuff

--- web/src/application/users/converters/users/createUserRequestConverter.ts
+++ web/src/application/users/converters/users/createUserRequestConverter.ts
@@ -1,7 +1,9 @@
 import {nameOf} from "../../../../framework/extras/typeUtils";
-import {UserInfo} from "../../../../model";
+import {ReferenceInfo, ReferenceType, UserInfo} from "../../../../model";
 import {Converter} from "../../../common/converters/converter";
 import {getDateWithoutTime} from "../../../../framework.visual/extras/utils/timeUtils";
+import {repoService} from "../../../serviceComposition";
+import {forEach} from "../../../../framework.visual/extras/utils/collectionUtils";
 
 export class CreateUserRequestConverter extends Converter<any,any>{
 
@@ -20,20 +22,33 @@ export class CreateUserRequestConverter extends Converter<any,any>{
             return result;
         }
 
+        let roles = [];
+        if (dictionary['role']) {
+            roles.push(dictionary['role']);
+        }
+        else {
+            let references = repoService.getAll(ReferenceInfo.class);
+            forEach(references, (referenceInfo: ReferenceInfo) => {
+                if (referenceInfo.type === ReferenceType.ROLE && referenceInfo.title.toUpperCase() == 'VIEWER') {
+                    roles.push(referenceInfo.id)
+                    return true;
+                }
+            });
+        }
+
         let serverUser = {
-            // id: id,
             dod_id: getTextValueOrDefault(nameOf<UserInfo>('dod_id'), '-1'),
             first_name: getTextValueOrDefault(nameOf<UserInfo>('first_name'), ''),
             middle_initial: '',
             last_name: getTextValueOrDefault(nameOf<UserInfo>('last_name'), ''),
             email_address: getTextValueOrDefault(nameOf<UserInfo>('email_address'), ''),
             phone_number: getTextValueOrDefault(nameOf<UserInfo>('phone_number'), ''),
-            dept_id: 1010,
-            preferred_results_view: 'Card'
-            // account_status: getTextValueOrDefault(nameOf<UserInfo>('account_status'), ''),
-            // roles: roles,
-            // approved_by: getTextValueOrDefault(nameOf<UserInfo>('approved_by'), ''),
-            // date_approved: getDateWithoutTime(new Date()),
+            dept_id: 1007,
+            preferred_results_view: 'Card',
+            account_status: 'Inactive',
+            roles: [1021],
+            approved_by: '',
+            date_approved: ''
         }
 
         return serverUser;
