f3d9330cb4d4a3fdd9cd00c3413f89e1005dda62
anthonylowther <anthonylowther@icloud.com>
2022-01-24 16:00:24 -0600

    adding deployment for hmi web

--- ci/deployment/deploy2docker.bat
+++ ci/deployment/deploy2docker.bat
@@ -1,1 +1,3 @@
-docker-compose -p tbe up -d --force-recreate --remove-orphans
+@REM docker-compose -p tbe up -d --force-recreate --remove-orphans
+
+docker-compose -f docker-compose-test.yml -f docker-compose-production.yml up -d --force-recreate --remove-orphans
