536c43ebbadc0a6c7ffc87d0bc05c0525c7230fb
anthony-at-526299150669 <HK9ZjUhHLOn3HqIzbZ+Dkxdf9ULSz/7XjR03rQLXBvQ=>
2022-01-28 17:30:09 -0600

    environment variables / ci

--- ci/copy2server.bat
+++ ci/copy2server.bat
@@ -1,2 +1,2 @@
 copy ../web/build/ ./ui /s /e
-scp -i .\bumed-web-srv.pem -r c:/Repos/km-hmi/docker-deploy ec2-user@18.253.190.212:/km_deployment/
+scp -i .\bumed-web-srv.pem -r c:/Repos/km-hmi/ci/ ec2-user@18.253.190.212:/km_deployment/
