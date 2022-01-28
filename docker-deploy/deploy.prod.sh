3c7c3cb8320bbe7ab45f05efaa95375caa421852
anthony-at-526299150669 <HK9ZjUhHLOn3HqIzbZ+Dkxdf9ULSz/7XjR03rQLXBvQ=>
2022-01-28 09:22:37 -0600

    environment variables / ci

---
+++ docker-deploy/deploy.prod.sh
@@ -0,0 +1,1 @@
+docker-compose --env-file=./env/env.prod -p tbe-prod up -d --force-recreate --remove-orphans
--- docker-deploy/deploy2docker.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f docker-compose.yml -p tbe up -d --force-recreate --remove-orphans
