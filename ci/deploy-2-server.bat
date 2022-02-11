d1871633888bb8396d16711531c2b2ed3e0765bf
anthony-at-526299150669 <HK9ZjUhHLOn3HqIzbZ+Dkxdf9ULSz/7XjR03rQLXBvQ=>
2022-02-11 15:51:57 -0600

    create km_deployment if not exists

--- ci/deploy-2-server.bat
+++ ci/deploy-2-server.bat
@@ -12,6 +12,7 @@ SET SSH_USER=%3
 
 
 @REM set permissions
+ssh -i %SSH_PEM% %SSH_USER% "sudo mkdir /km_deployment/"
 ssh -i %SSH_PEM% %SSH_USER% "sudo setfacl -R -m u:ec2-user:rwx /km_deployment/"
 @REM clear deployment directory
 ssh -i %SSH_PEM% %SSH_USER% "sudo rm -r /km_deployment/*"
