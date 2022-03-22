5dc88896edf0b19b61a51c17932af13dfd93a4b7
iangwaltney <ian@tektonux.com>
2022-03-22 13:41:25 -0500

    statProvider tests

---
+++ web/tests/statProvider.test.ts
@@ -0,0 +1,109 @@
+import {
+    AuthenticationService,
+    AuthorizationService,
+    PermissionProvider,
+    RoleProvider, StatProvider,
+    UserProvider
+} from "../src/app.core";
+import {HttpService, LogService, RepositoryService} from "../src/framework.core/services";
+import {
+    IEntityProvider,
+    IFetchAdapter,
+    IHttpService,
+    ILogService,
+    IRepositoryService,
+    IStorage
+} from "../src/framework.core.api";
+import {IAuthenticationService, IAuthorizationService, IUserProvider} from "../src/app.core.api";
+import {AppDataStore} from "../src/framework.core/redux/reduxStore";
+import {PermissionInfo, ReferenceType, RoleInfo, StatInfo} from "../src/app.model";
+import {NodeFetchAdapter} from "../src/framework.core/networking/nodeFetchAdapter";
+
+
+const statProvider: IEntityProvider<StatInfo> = new StatProvider();
+
+beforeAll(() => {
+    const appDataStore: IStorage = new AppDataStore();
+    const logService: ILogService = new LogService();
+    const repoService: IRepositoryService = new RepositoryService();
+    const fetchAdapter: IFetchAdapter = new NodeFetchAdapter();
+    const httpService: IHttpService = new HttpService();
+
+    // create the entity providers
+    const roleProvider: IEntityProvider<RoleInfo> = new RoleProvider();
+    const userProvider: IUserProvider = new UserProvider();
+    const permissionProvider: IEntityProvider<PermissionInfo> = new PermissionProvider();
+
+    // create the application services
+    const authenticationService: IAuthenticationService = new AuthenticationService();
+    const authorizationService: IAuthorizationService = new AuthorizationService();
+
+
+    // set references and start framework plugins
+    // stats
+    statProvider.setLogService(logService);
+    statProvider.setRepositoryService(repoService);
+    statProvider.setHttpService(httpService);
+    statProvider.start();
+    // log service
+    logService.configure();
+    logService.start();
+    // repo service
+    repoService.setLogService(logService);
+    repoService.setStorage(appDataStore);
+    repoService.start();
+    // http service
+    httpService.setLogService(logService);
+    httpService.setAuthenticationService(authenticationService);
+    httpService.setFetchAdapter(fetchAdapter)
+    httpService.start();
+
+    // set references and start entity providers
+    // roles
+    roleProvider.setLogService(logService);
+    roleProvider.setRepositoryService(repoService);
+    roleProvider.setHttpService(httpService);
+    roleProvider.start();
+    // users
+    userProvider.setLogService(logService);
+    userProvider.setRepositoryService(repoService);
+    userProvider.setHttpService(httpService);
+    userProvider.setRoleProvider(roleProvider);
+    userProvider.start();
+    // permissions
+    permissionProvider.setLogService(logService);
+    permissionProvider.setRepositoryService(repoService);
+    permissionProvider.setHttpService(httpService);
+    permissionProvider.start();
+
+    // set references and start application services
+    // authentication service
+    authenticationService.setLogService(logService);
+    authenticationService.setRepositoryService(repoService);
+    authenticationService.setUserProvider(userProvider);
+    authenticationService.setAppDataStore(appDataStore);
+    authenticationService.setAuthorizationService(authorizationService);
+    authenticationService.start();
+    // authorization service
+    authorizationService.setLogService(logService);
+    authorizationService.setRepositoryService(repoService);
+    authorizationService.setPermissionProvider(permissionProvider);
+    authorizationService.setUserProvider(userProvider);
+    authorizationService.setAppDataStore(appDataStore);
+    authorizationService.setAuthenticationService(authenticationService);
+    authorizationService.start();
+})
+
+describe("Test setup", () => {
+    it("referenceProvider is not null", () => {
+        expect(statProvider).not.toBeNull();
+    });
+})
+
+describe("getAll", () => {
+    it("gets all stats", () => {
+        return statProvider.getAll().then(statInfos => {
+            expect(statInfos.length).not.toBe(0);
+        });
+    });
+});
