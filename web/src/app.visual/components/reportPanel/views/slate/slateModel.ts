381471e77b2ca5270cddc1bb044aefebbcac9290
anthony-at-526299150669 <HK9ZjUhHLOn3HqIzbZ+Dkxdf9ULSz/7XjR03rQLXBvQ=>
2022-03-28 11:42:49 -0500

    reports ui - text alignment, numbered/bulleted lists, font size

--- web/src/app.visual/components/reportPanel/views/slate/slateModel.ts
+++ web/src/app.visual/components/reportPanel/views/slate/slateModel.ts
@@ -1,6 +1,7 @@
 import {BaseEditor} from "slate";
 import {ReactEditor} from "slate-react";
 import {HistoryEditor} from "slate-history";
+import {ButtonProps} from "../../../../theme/widgets/button/buttonModel";
 
 declare module 'slate' {
     interface CustomTypes {
@@ -8,7 +9,7 @@ declare module 'slate' {
     }
 }
 
-export type NodeProps = {
+export type ElementProps = {
     attributes: any[],
     children: JSX.Element,
     element: {
@@ -17,7 +18,7 @@ export type NodeProps = {
     }
 }
 
-export type MarkProps = {
+export type LeafProps = {
     attributes: any[],
     children: JSX.Element,
     leaf: {
@@ -25,5 +26,33 @@ export type MarkProps = {
         code: boolean,
         italic: boolean,
         underline: boolean
+        fontSize: string
     }
+}
+
+export type SlateEditorProps = {
+    onChange: (value: any) => void;
+}
+
+export type SlateEditorState = {
+
+}
+
+export enum LIST_TYPE {
+    'numbered-list'='numbered-list',
+    'bulleted-list'='bulleted-list'
+}
+export enum TEXT_ALIGN_TYPE {
+    'left'='left',
+    'center'='center',
+    'right'='right',
+    'justify'='justify'
+}
+
+export type FontSizeInputProps = ButtonProps & {
+
+}
+
+export type BlockButtonProps = ButtonProps & {
+    format: string
 }
--- web/src/app.visual/theme/widgets/comboBox/comboBox.tsx
+++ web/src/app.visual/theme/widgets/comboBox/comboBox.tsx
@@ -170,7 +170,7 @@ class ComboBox extends Component<ComboBoxProps, ComboBoxState> {
                 }
 
                 comboBoxItems.push(
-                    <ComboBoxItem className={'d-flex'} key={itemId} title={itemTitle} multiSelect={multiSelect}
+                    <ComboBoxItem className={'d-flex'} key={itemId} title={itemTitle || itemId} multiSelect={multiSelect}
                                   onClick={() => this._onSelectHandler(itemId || "")} selected={value} readonly={readonly}/>
                 )
             });
